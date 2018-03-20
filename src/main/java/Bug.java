bd0b01f880d41e2a00fa27fc2492818fa1166353
Korbinian Riedhammer <korbinian.riedhammer@fh-rosenheim.de>
2018-03-20 16:56:03 +0100

    Musterloesung

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -6,9 +6,12 @@ public class Bug {
 	 * @throws IllegalArgumentException if referenced array is null or empty
 	 */
 	public static int minimum(int[] a) throws IllegalArgumentException {
+		if (a == null || a.length == 0)
+			throw new IllegalArgumentException();
+
 		int m = a[0];
 		for (int i = 1; i < a.length; i++) {
-			if (a[i] > m) {
+			if (a[i] < m) {
 				m = a[i];
 			}
 		}
