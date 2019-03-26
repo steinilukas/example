04550720e238044ac18b72692e967ba5c652473f
steinilukas <lukas.steinbrueck@gmail.com>
2019-03-26 11:36:39 +0100

    gelöst

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -6,13 +6,22 @@ public class Bug {
 	 * @throws IllegalArgumentException if referenced array is null or empty
 	 */
 	public static int minimum(int[] a) throws IllegalArgumentException {
-		int m = a[0];
-		for (int i = 1; i < a.length; i++) {
-			if (a[i] > m) {
-				m = a[i];
-			}
+		if (a == null){
+			throw new IllegalArgumentException();
 		}
+		if (a.length <= 0){
+			throw new IllegalArgumentException();
+		}
+		else{
 
-		return m;
+			int m = a[0];
+			for (int i = 1; i < a.length; i++) {
+				if (a[i] < m) {
+					m = a[i];
+				}
+			}
+
+			return m;
+		}
 	}
 }
